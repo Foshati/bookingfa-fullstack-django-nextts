d02e0bc92d1c640b2cbd59ea87c383e13922bce0
Foshati <amirrezafoshati@gmail.com>
2024-09-12 23:47:54 +0330

    create and start project django and fix next.js

--- frontend-nextts/src/app/page.tsx
+++ frontend-nextts/src/app/page.tsx
@@ -1,7 +1,10 @@
+import {Button} from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main>
-      <p className='text-primary'>hello fa</p>
+      <p className='text-red-500'>hello fa</p>
+      <Button>Click me</Button>
     </main>
   );
 }
