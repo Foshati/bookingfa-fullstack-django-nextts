92f66d3d136fa8a4e04e34008b2d232f0e35681a
Foshati <amirrezafoshati@gmail.com>
2024-10-29 23:44:44 +0330

    start and fix project and start create navbar

--- frontend-nextts/src/app/page.tsx
+++ frontend-nextts/src/app/page.tsx
@@ -3,7 +3,7 @@ import {Button} from '@/components/ui/button';
 export default function Home() {
   return (
     <main>
-      <p className='text-red-500'>hello fa</p>
+      <p className='text-bookingfa'>hello fa</p>
       <Button>Click me</Button>
     </main>
   );
