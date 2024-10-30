5aebdc8964fe7c2221003981b4f3651efdde2000
Foshati <amirrezafoshati@gmail.com>
2024-10-30 23:49:45 +0330

    fix navbar

--- frontend-nextts/components/layout/Navbar.tsx
+++ frontend-nextts/components/layout/Navbar.tsx
@@ -1,5 +1,18 @@
-export default function Navbar() {
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = async () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className='fixed left-0 top-0 z-10 w-full border-b bg-white py-6'>
+      <div className='mx-auto max-w-[1500px] px-6'>
+        <div className='flex items-center justify-between'>
+          <Link href='/'>
+            <Image src='/logo.png' alt='DjangoBnb logo' width={180} height={38} />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
